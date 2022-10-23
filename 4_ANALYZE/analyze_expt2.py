40a5ae701c81f27b185f687fbeb69c1bba5fb17a
Greta Tuckute <gretatu@mit.edu>
2022-10-23 19:41:54 -0400

    add additional analyses to expt 2 (orth and freq/meaning)

--- 4_ANALYZE/analyze_expt2.py
+++ 4_ANALYZE/analyze_expt2.py
@@ -2,10 +2,12 @@ from utils import *
 
 ## SETTINGS ##
 Q0 = True # subject consistency
-Q1 = True # accuracy metrics
+Q1 = False # accuracy metrics
 Q2 = True # all baseline models
 Q3 = True # models for baseline + ONE additional predictor
 Q4 = True # forward-backward selection
+Q5 = False # orthographic/phonological neighborhood
+Q6 = True # disambiguate frequency vs meaning
 
 posthoc_stats = False
 
@@ -36,7 +38,7 @@ if __name__ == '__main__':
 									   CI=95,
 									   result_dir=RESULTDIR,
 									   save_subfolder='1_acc_metrics',
-									   save_str='expt1_acc_metrics',
+									   save_str='expt2_acc_metrics',
 									   save=save)
 		
 	## Fit linear models ##
@@ -166,6 +168,136 @@ if __name__ == '__main__':
 			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_google_ngram_'
 					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
 				fh.write(m_google_ngram.summary().as_text())
+
+
+		## Topic variability (and document frequency) ##
+
+		# Run the topic variability predictors. However, this one has NaNs, so first obtain the dataset without NaNs:
+		df_no_nan_topic, acc1_no_nan_topic, acc2_no_nan_topic, nan_info_no_nan_topic = \
+			drop_nans_from_df(df=df, acc1=acc1, acc2=acc2,
+							  predictors=['log_topic_variability', 'log_document_frequency'])
+
+		# Save this version of the dataset
+		if save:
+			df_no_nan_topic.to_csv(f'{RESULTDIR}/'
+					  f'data_with_preprocessed_cols_used_for_analyses/'
+					  f'{fname.split("/")[-1].split(".")[0]}_preprocessed_cols_no_nan_topic.csv')
+
+		df_topic = get_cv_score(df=df_no_nan_topic,
+							   acc1=acc1_no_nan_topic, acc2=acc2_no_nan_topic,
+							   save=save, result_dir=RESULTDIR,
+							   model_name='topicvar',
+							   predictors=['log_topic_variability'], save_subfolder=save_subfolder)
+
+		df_docfreq = get_cv_score(df=df_no_nan_topic,
+								   acc1=acc1_no_nan_topic, acc2=acc2_no_nan_topic,
+								   save=save, result_dir=RESULTDIR,
+								   model_name='docfreq',
+								   predictors=['log_document_frequency'], save_subfolder=save_subfolder)
+
+
+		# Fit models using baseline model, but with limited set of words (due to the NaNs)
+		df_baseline_human_no_nans_topic = get_cv_score(df=df_no_nan_topic,
+													   acc1=acc1_no_nan_topic, acc2=acc2_no_nan_topic,
+													   save=save, result_dir=RESULTDIR,
+													   model_name='baseline_human_no_nans_topic',
+													   predictors=['num_meanings_human', 'num_synonyms_human'],
+													   save_subfolder=save_subfolder)
+
+		# Model with baseline predictors and topic variability
+		df_baseline_human_topic = get_cv_score(df=df_no_nan_topic,
+											   acc1=acc1_no_nan_topic, acc2=acc2_no_nan_topic,
+											   save=save, result_dir=RESULTDIR,
+											   model_name='baseline_human_topic',
+											   predictors=['num_meanings_human', 'num_synonyms_human',
+											   				'log_topic_variability'],
+											   save_subfolder=save_subfolder)
+
+		# Also run CD on the limited set
+		df_CD_no_nans_topic = get_cv_score(df=df_no_nan_topic,
+										   acc1=acc1_no_nan_topic, acc2=acc2_no_nan_topic,
+										   save=save, result_dir=RESULTDIR,
+										   model_name='CD_no_nans_topic',
+										   predictors=['log_subtlex_cd'], save_subfolder=save_subfolder)
+
+		# Merge all CV dfs
+		df_baseline_human_topic_concat = pd.concat([df_meanings_human, df_synonyms_human, df_baseline_human,
+													df_CD,
+													df_topic, df_docfreq, df_baseline_human_no_nans_topic, df_baseline_human_topic, df_CD_no_nans_topic],)
+
+		if save:  # store the concatenated results across all baseline models
+			df_baseline_human_topic_concat.to_csv(
+				f'{RESULTDIR}/{save_subfolder}/'
+				f'cv_summary_preds/'
+				f'across-models_df_cv_NAME-baseline-human-topic_'
+				f'demeanx-True_demeany-True_permute-False_{date_tag}.csv')
+
+
+		# Fit models on the full dataset for model statistics
+
+		# Human baseline
+		m_baseline_human = smf.ols('acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean', data=df_no_nan_topic).fit()
+
+		# Topic variability
+		m_topic = smf.ols('acc_demean ~ log_topic_variability_demean', data=df_no_nan_topic).fit()
+
+		# Document frequency
+		m_docfreq = smf.ols('acc_demean ~ log_document_frequency_demean', data=df_no_nan_topic).fit()
+
+		# Topic variability and human baseline
+		m_baseline_human_topic = smf.ols('acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean + '
+										 'log_topic_variability_demean', data=df_no_nan_topic).fit()
+
+
+		## ANOVA COMPARISON WITH BASELINE MODELS ##
+
+		# Compare Mem ~ synonyms + meanings WITH Mem ~ synonyms + meanings + orth_neighborhood
+		comp_baseline_human_orth = sm.stats.anova_lm(m_baseline_human, m_baseline_human_topic)
+		comp_baseline_human_orth['model'] = 'acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean'
+		comp_baseline_human_orth[
+			'model_add_predictor'] = 'acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean + log_topic_variability_demean'
+
+
+		# Package the ANOVA model comparisons into one df
+		df_comp_anova = comp_baseline_human_orth
+
+		# Create 'comparison_index' column (two rows per comparison, so repeat the index twice)
+		df_comp_anova['comparison_index'] = (np.repeat(np.arange(len(df_comp_anova) / 2), 2))
+
+		# Reorganize columns: comparison_index, model, model_add_predictor, F, Pr(>F), ss_diff, df_diff, ssr
+		df_comp_anova = df_comp_anova[
+			['comparison_index', 'model', 'model_add_predictor', 'F', 'Pr(>F)', 'ss_diff', 'df_diff', 'ssr']]
+
+
+		if save:
+			df_comp_anova.to_csv(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_comp_anova_'
+								 f'NAME-baseline-human-topic_'
+								 f'demeanx-True_demeany-True_permute-False_{date_tag}.csv')
+
+
+			# Log
+			# Human baseline
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_baseline_human_'
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_baseline_human.summary().as_text())
+
+			# Topic variability
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_topic_'
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_topic.summary().as_text())
+
+			# Document frequency
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_docfreq_'
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_docfreq.summary().as_text())
+
+			# Topic variability and human baseline
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_baseline_human_topic_'
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_baseline_human_topic.summary().as_text())
+
+
+
 		
 	## 3. Do additional factors contribute to word memorability?
 	if Q3:
@@ -292,8 +424,249 @@ if __name__ == '__main__':
 					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
 				fh.write(m_stepwise.summary().as_text())
 	
-	
-	
+
+	## Orthographical / phonological neighborhood size ##
+	if Q5:
+		save_subfolder = '5_orth_phon_neighborhood'
+
+		# Get a version of the df and acc files without nans
+		df_no_nan_orth_phon, acc1_no_nan_orth_phon, acc2_no_nan_orth_phon, nan_info_no_nan_orth_phon = \
+			drop_nans_from_df(df=df, acc1=acc1, acc2=acc2, predictors=['log_orthographic_neighborhood_size', 'log_phonological_neighborhood_size'])
+
+		# Save this version of the dataset
+		if save:
+			df_no_nan_orth_phon.to_csv(f'{RESULTDIR}/'
+					  f'data_with_preprocessed_cols_used_for_analyses/'
+					  f'{fname.split("/")[-1].split(".")[0]}_preprocessed_cols_no_nan_orth_phon.csv')
+
+
+
+		df_orth = get_cv_score(df=df_no_nan_orth_phon,
+							   acc1=acc1_no_nan_orth_phon, acc2=acc2_no_nan_orth_phon,
+							   save=save, result_dir=RESULTDIR,
+							   model_name='orth_neighborhood',
+							   predictors=['log_orthographic_neighborhood_size'], save_subfolder=save_subfolder)
+
+		df_phon = get_cv_score(df=df_no_nan_orth_phon,
+							   acc1=acc1_no_nan_orth_phon, acc2=acc2_no_nan_orth_phon,
+							   save=save, result_dir=RESULTDIR,
+							   model_name='phon_neighborhood',
+							   predictors=['log_phonological_neighborhood_size'], save_subfolder=save_subfolder)
+
+
+		# Drop the words (using nan_indices) that were not included in the neighborhood size calculation
+		df_baseline_human_no_nans_orth_phon = get_cv_score(df=df_no_nan_orth_phon,
+														   acc1=acc1_no_nan_orth_phon, acc2=acc2_no_nan_orth_phon,
+														   save=save, result_dir=RESULTDIR,
+														   model_name='baseline_human_no_nans_orth_phon',
+														   predictors=['num_meanings_human', 'num_synonyms_human'], save_subfolder=save_subfolder)
+
+		# Concatenate the results
+		df_orth_phon_concat = pd.concat([df_orth, df_phon, df_baseline_human_no_nans_orth_phon], axis=0)
+
+		# Fit models on the full dataset for model statistics
+
+		# Human baseline
+		m_baseline_human = smf.ols('acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean', data=df_no_nan_orth_phon).fit()
+
+		# Orthographic neighborhood size
+		m_orth = smf.ols('acc_demean ~ log_orthographic_neighborhood_size_demean', data=df_no_nan_orth_phon).fit()
+
+		# Phonological neighborhood size
+		m_phon = smf.ols('acc_demean ~ log_phonological_neighborhood_size_demean', data=df_no_nan_orth_phon).fit()
+
+		# Human baseline plus orthographic neighborhood size
+		m_baseline_human_orth = smf.ols('acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean + '
+										'log_orthographic_neighborhood_size_demean', data=df_no_nan_orth_phon).fit()
+
+		# Human baseline plus phonological neighborhood size
+		m_baseline_human_phon = smf.ols('acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean + '
+										'log_phonological_neighborhood_size_demean', data=df_no_nan_orth_phon).fit()
+
+		# Human baseline plus orthographic and phonological neighborhood size
+		m_baseline_human_orth_phon = smf.ols('acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean + '
+											 'log_orthographic_neighborhood_size_demean + '
+											 'log_phonological_neighborhood_size_demean', data=df_no_nan_orth_phon).fit()
+
+
+		## ANOVA COMPARISON WITH BASELINE MODELS ##
+
+		# Compare Mem ~ synonyms + meanings WITH Mem ~ synonyms + meanings + orth_neighborhood
+		comp_baseline_human_orth = sm.stats.anova_lm(m_baseline_human, m_baseline_human_orth)
+		comp_baseline_human_orth['model'] = 'acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean'
+		comp_baseline_human_orth[
+			'model_add_predictor'] = 'acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean + log_orthographic_neighborhood_size_demean'
+
+		# Compare Mem ~ synonyms + meanings WITH Mem ~ synonyms + meanings + phon_neighborhood
+		comp_baseline_human_phon = sm.stats.anova_lm(m_baseline_human, m_baseline_human_phon)
+		comp_baseline_human_phon['model'] = 'acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean'
+		comp_baseline_human_phon[
+			'model_add_predictor'] = 'acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean + log_phonological_neighborhood_size_demean'
+
+		# Compare Mem ~ synonyms + meanings WITH Mem ~ synonyms + meanings + orth_neighborhood + phon_neighborhood
+		comp_baseline_human_orth_phon = sm.stats.anova_lm(m_baseline_human, m_baseline_human_orth_phon)
+		comp_baseline_human_orth_phon['model'] = 'acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean'
+		comp_baseline_human_orth_phon[
+			'model_add_predictor'] = 'acc_demean ~ num_meanings_human_demean + num_synonyms_human_demean + log_orthographic_neighborhood_size_demean + log_phonological_neighborhood_size_demean'
+
+		# Package the ANOVA model comparisons into one df
+		df_comp_anova = pd.concat([comp_baseline_human_orth, comp_baseline_human_phon, comp_baseline_human_orth_phon])
+
+		# Create 'comparison_index' column (two rows per comparison, so repeat the index twice)
+		df_comp_anova['comparison_index'] = (np.repeat(np.arange(len(df_comp_anova) / 2), 2))
+
+		# Reorganize columns: comparison_index, model, model_add_predictor, F, Pr(>F), ss_diff, df_diff, ssr
+		df_comp_anova = df_comp_anova[
+			['comparison_index', 'model', 'model_add_predictor', 'F', 'Pr(>F)', 'ss_diff', 'df_diff', 'ssr']]
+
+		if save:
+			df_comp_anova.to_csv(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_comp_anova_'
+								 f'NAME-baseline-human-orth-phon_'
+								 f'demeanx-True_demeany-True_permute-False_{date_tag}.csv')
+
+		if save:  # store the concatenated results across all baseline models
+
+			df_orth_phon_concat.to_csv(
+				f'{RESULTDIR}/{save_subfolder}/'
+				f'cv_summary_preds/'
+				f'across-models_df_cv_NAME-baseline-human-orth-phon_'
+				f'demeanx-True_demeany-True_permute-False_{date_tag}.csv')
+
+			# Log
+			# Human baseline
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_baseline_human_'
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_baseline_human.summary().as_text())
+
+			# Orthographic neighborhood size
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_orth_'
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_orth.summary().as_text())
+
+			# Phonological neighborhood size
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_phon_'
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_phon.summary().as_text())
+
+			# Human baseline plus orthographic neighborhood size
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_baseline_human_orth_' 
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_baseline_human_orth.summary().as_text())
+
+			# Human baseline plus phonological neighborhood size
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_baseline_human_phon_' 
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_baseline_human_phon.summary().as_text())
+
+			# Human baseline plus orthographic and phonological neighborhood size
+			with open(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_NAME-m_baseline_human_orth_phon_'
+					  f'demeanx-True_demeany-True_permute-False_{date_tag}.txt', 'w') as fh:
+				fh.write(m_baseline_human_orth_phon.summary().as_text())
+
+
+	#### Disambiguate frequency vs meaning ####
+	if Q6:
+		save_subfolder = '6_freq_vs_meanings'
+
+		freq_metrics = ['google_ngram_frequency']
+
+		for freq_metric in freq_metrics:
+			## Show frequency distribution of data
+			fig, ax = plt.subplots(figsize=(6, 6))
+			ax.hist(df[freq_metric], bins=20, color='black')
+			ax.set_xlabel(freq_metric)
+			ax.set_ylabel('Count')
+			ax.set_title(f'Distribution of {freq_metric} frequency')
+			plt.tight_layout()
+			plt.show()
+
+			# Divide into 3 bins based on frequency with equal number of items in each bin
+			df['frequency_bin'] = pd.qcut(df.rank(method='first')[freq_metric], 3,
+													  labels=['low', 'medium', 'high'])
+
+
+			# Get count of number of items in each bin and the mean and standard deviation of the frequency
+			df_freq_bin = df.groupby('frequency_bin').agg(
+				{'log_subtlex_frequency': ['count', 'mean', 'std']})
+			print(df_freq_bin)
+
+			# Get count of number of items in each bin and the mean and standard deviation of the number of meanings
+			df_meanings_bin = df.groupby('frequency_bin').agg(
+				{'num_meanings_human': ['count', 'mean', 'std']})
+			print(df_meanings_bin)
+
+			# Get count of number of items in each bin and the mean and standard deviation of the number of synonyms
+			df_synonyms_bin = df.groupby('frequency_bin').agg(
+				{'num_synonyms_human': ['count', 'mean', 'std']})
+			print(df_synonyms_bin)
+
+			# Store these stats
+			if save:
+				df_stats = pd.concat([df_freq_bin, df_meanings_bin, df_synonyms_bin], axis=1)
+				df_stats.to_csv(f'{RESULTDIR}/{save_subfolder}/full_summary/summary_stats_freq-bins-{freq_metric}_' 
+								f'demeanx-True_demeany-True_permute-False_{date_tag}.csv')
+
+			# Run CV regression on data from each bin
+			# First, create the new dataframes for each bin (we need to make sure the accs are also dropped in the same way)
+
+			# Create three columns that have 1 if the item is in that bin, and 0 otherwise
+			df['frequency_bin_low'] = np.where(df['frequency_bin'] == 'low', 1, np.nan)
+			df['frequency_bin_medium'] = np.where(df['frequency_bin'] == 'medium', 1, np.nan)
+			df['frequency_bin_high'] = np.where(df['frequency_bin'] == 'high', 1, np.nan)
+
+
+			# The following will generate dfs with values only for the low, medium, and high bins
+			df_low, acc1_low, acc2_low, nan_info_low = \
+				drop_nans_from_df(df=df, acc1=acc1, acc2=acc2,
+								  predictors=['frequency_bin_low'])
+
+			df_medium, acc1_medium, acc2_medium, nan_info_medium = \
+				drop_nans_from_df(df=df, acc1=acc1, acc2=acc2,
+								  predictors=['frequency_bin_medium'])
+
+			df_high, acc1_high, acc2_high, nan_info_high = \
+				drop_nans_from_df(df=df, acc1=acc1, acc2=acc2,
+								  predictors=['frequency_bin_high'])
+
+			# Run CV regression on each bin
+
+			## Human baseline
+			# Low
+			df_human_baseline_low = get_cv_score(df=df_low,
+								   acc1=acc1_low, acc2=acc2_low,
+								   save=save, result_dir=RESULTDIR,
+								   model_name='baseline_human_low_freq_bin',
+								   predictors=['num_meanings_human', 'num_synonyms_human'], save_subfolder=save_subfolder)
+
+			# Medium
+			df_human_baseline_medium = get_cv_score(df=df_medium,
+									   acc1=acc1_medium, acc2=acc2_medium,
+									   save=save, result_dir=RESULTDIR,
+									   model_name='baseline_human_medium_freq_bin',
+									   predictors=['num_meanings_human', 'num_synonyms_human'], save_subfolder=save_subfolder)
+
+			# High
+			df_human_baseline_high = get_cv_score(df=df_high,
+									 acc1=acc1_high, acc2=acc2_high,
+									 save=save, result_dir=RESULTDIR,
+									 model_name='baseline_human_high_freq_bin',
+									 predictors=['num_meanings_human', 'num_synonyms_human'], save_subfolder=save_subfolder)
+
+
+			# Merge all CV dfs together
+			df_freq_bins_concat = pd.concat([df_human_baseline_low, df_human_baseline_medium, df_human_baseline_high])
+
+			if save:
+				df_freq_bins_concat.to_csv(
+					f'{RESULTDIR}/{save_subfolder}/'
+					f'cv_summary_preds/'
+					f'across-models_df_cv_NAME-freq-bins-{freq_metric}_'
+					f'demeanx-True_demeany-True_permute-False_{date_tag}.csv')
+
+
+
+
+
 	if posthoc_stats:
 		# Test human baseline vs CD
 		bootstrap_wrapper(result_dir=RESULTDIR,
